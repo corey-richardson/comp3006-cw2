8787d8eb843e8c5725354671f2706d42e156c2ba
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-28 14:44:37 +0000

    Fix Post API routes using incorrect HTTP methods

--- SourceCode/backend/routes/posts.js
+++ SourceCode/backend/routes/posts.js
@@ -1,6 +1,5 @@
-// Routes relative to "/api/posts"
-
 const express = require("express");
+const router = express.Router();
 
 const {
     getPosts,
@@ -10,21 +9,11 @@ const {
     updatePost
 } = require("../controllers/postController");
 
-const router = express.Router();
-
-// GET
+// Routes relative to "/api/posts"
 router.get("/", getPosts);
-
-// GET
 router.get("/:id", getPost);
-
-// POST
-router.get("/", createPost);
-
-// DELETE
-router.get("/:id", deletePost);
-
-// UPDATE
-router.get("/:id", updatePost);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
+router.patch("/:id", updatePost);
 
 module.exports = router;
