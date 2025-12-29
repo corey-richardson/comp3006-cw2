779db171f76f9b5d5cb213d981d54b55c323c285
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-29 18:28:30 +0000

    Updated `Navbar` component; authentication state dependent

--- SourceCode/frontend/src/components/Navbar.jsx
+++ SourceCode/frontend/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+    const { user } = useAuthContext();
+    
     return ( 
-        <header>
-            <div className="container">
-                <Link to="/"><h1>Home</h1></Link>
+        <header className={styles.header}>
+            <div className={styles.navbarContainer}>
+                <h1 className={styles.title}>Social Media App</h1>
+            
+                <nav>
+                    { user ? (
+                        <div className={styles.navLinks}>
+                            <Link to="/">Home Feed</Link>
+                            <Link to={`/profile/${user.username}`}>{user.username}</Link>
+                        </div>
+                    ) : (
+                        <div className={styles.navLinks}>
+                            <Link to="/login">Login</Link>
+                            <Link to="/login">Signup</Link>
+                        </div>
+                    )}
+                </nav>
             </div>
         </header>
      );
