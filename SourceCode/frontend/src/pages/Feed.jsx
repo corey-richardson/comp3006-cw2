3180275e7dd9efbd1436187226b2e7f0e1013176
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-30 22:32:36 +0000

    Style Feed page; CLSX conditional styling for current active feed; `react-lucide` icons

--- SourceCode/frontend/src/pages/Feed.jsx
+++ SourceCode/frontend/src/pages/Feed.jsx
@@ -1,6 +1,10 @@
 import { usePosts } from "../hooks/usePosts";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { Earth, NotebookPen, UserRoundCheck  } from "lucide-react";
 import PostCard from "../components/PostCard";
+import styles from "../styles/Feed.module.css";
 
 const Feed = () => {
     const { posts, hasMore, fetchPage } = usePosts();
@@ -20,31 +24,48 @@ const Feed = () => {
     }
 
     return ( 
-        <div className="feed">
-            <h2>Feed</h2>
+        <div className={styles.feed}>
+            <header className={styles.feedHeader}>
+                <h2>Feed</h2>
+            </header>
 
-            <nav className="feed-types-selectors">
-                <button onClick={() => setFeedType("global")}>Global</button>
-                <button onClick={() => setFeedType("following")}>Following</button>
+            <nav className={styles.feedTypeSelectors}>
+                <button 
+                    onClick={() => setFeedType("global")} 
+                    className={clsx(styles.selectorItem, { [styles.active]: feedType === "global" })}
+                >
+                    {<Earth size={20} />} &nbsp; Global
+                </button>
+
+                <button 
+                    onClick={() => setFeedType("following")}
+                    className={clsx(styles.selectorItem, { [styles.active]: feedType === "following" })}
+                >
+                    {<UserRoundCheck size={20} />} &nbsp; Following
+                </button>
+
+                <Link to="/posts/new" className={styles.selectorItem}>
+                    {<NotebookPen size={20} />} &nbsp; New Post
+                </Link>
             </nav>
 
-            <div className="posts-list">
+            <div className={styles.postList}>
                 { posts && posts.map((post) => (
                     <PostCard key={post._id} post={post} />
                 ))}
 
                 { posts && posts.length === 0 && (
-                    <p>No posts yet!</p>
+                    <p className="small centred">No posts yet!</p>
                 )}
 
                 { hasMore && (
-                    <button onClick={loadMore} className="load-more-button">
+                    <button onClick={loadMore} className={styles.loadMoreButton}>
                         Load More Posts
                     </button>
                 )}
             </div>
         </div>
-     );
+    );
 }
  
 export default Feed;
