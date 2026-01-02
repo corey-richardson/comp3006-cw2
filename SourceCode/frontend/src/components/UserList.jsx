cb316707d9c009a91cdd76c7697559f132662dea
Corey Richardson <richardsoncorey04@gmail.com>
2026-01-02 16:47:12 +0000

    added styling to UserList

--- SourceCode/frontend/src/components/UserList.jsx
+++ SourceCode/frontend/src/components/UserList.jsx
@@ -3,6 +3,9 @@ import { Link, useParams } from "react-router-dom";
 
 import FollowButton from "./FollowButton";
 
+import profileStyles from '../styles/Profile.module.css';
+import listStyles from '../styles/UserList.module.css';
+
 const UserList = ({ users, type }) => {
     const { username } = useParams();
 
@@ -11,32 +14,32 @@ const UserList = ({ users, type }) => {
         : `Users who follow @${ username } (${users.length})`;
 
     return (
-        <div className="listContainer">
-            <h2>{ title }</h2>
+        <div className={profileStyles.container}>
+            <h2 className={ listStyles.title }>{ title }</h2>
 
             { users && users.length > 0 ? (
-                <ul className="list">
+                <ul className={ listStyles.list }>
                     { users.map((user) => {
                         const target = type === "followers" ? user.follower_id : user.following_id;
                         if (!target) return null;
 
                         return (
-                            <li key={user._id} className="userItem">
-                                <div className="userCard">
-                                    <div className="avatar">
+                            <li key={user._id} className={ listStyles.userItem }>
+                                <div className={ listStyles.userCard }>
+                                    <div className={` ${profileStyles.avatar} ${listStyles.avatarExtended} `}>
                                         <User size={20} />
                                     </div>
 
-                                    <div className="userDetails">
+                                    <div className={ listStyles.userDetails }>
                                         <Link to={`/profile/${target.username}`}>
-                                            <span className="fullName">
+                                            <span className={ listStyles.fullName }>
                                                 { target.firstName || "Anonymous" } { target.lastName || "" }
                                             </span>
-                                            <span className="handle">@{ target.username }</span>
+                                            <span className={profileStyles.handle}>@{ target.username }</span>
                                         </Link>
                                     </div>
 
-                                    <div className="actions">
+                                    <div className={ listStyles.actions }>
                                         <FollowButton targetUser={target._id} size={24} />
                                     </div>
                                 </div>
